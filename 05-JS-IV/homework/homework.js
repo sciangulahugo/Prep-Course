51ec49383afe181bbd45389e4b1836bf95cbe5b9
sciangulahugo <sciangulahugo@gmail.com>
2022-08-04 14:03:14 -0300

    hola danna

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,13 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      return 'Meow!';
+    }
+  }
 }
 
 function agregarPropiedad (objeto, property) {
